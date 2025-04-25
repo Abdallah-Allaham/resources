e907a3d10fd4289e05a551a93cec3f55a6441c3f
Abdallah-Allaham <allham.abod@gmail.com>
2025-04-25 23:09:32 +0300

    nothing to be commited

---
+++ src/Div3/HelpfulMaths.java
@@ -0,0 +1,45 @@
+package Div3;
+
+import java.util.Scanner;
+
+public class HelpfulMaths {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        String sum=in.next();
+        String[] num=sum.split("\\+");
+
+        int numOfA=0,numOfB=0,numOfC=0;
+
+        for(int i=0;i<sum.length();i++){
+            switch (sum.charAt(i)){
+                case '1':
+                    numOfA++;
+                    break;
+                case '2':
+                    numOfB++;
+                    break;
+                case '3':
+                    numOfC++;
+                    break;
+            }
+        }
+
+        String newSum="";
+        for (int i=0;i<numOfA;i++){
+            newSum += "1+";
+        }
+
+        for (int i=0;i<numOfB;i++){
+            newSum += "2+";
+        }
+
+        for (int i=0;i<numOfC;i++){
+            newSum += "3+";
+        }
+
+        newSum = newSum.substring(0,newSum.length()-1);
+
+        System.out.println(newSum);
+
+    }
+}
---
+++ src/Div3/PetyaAndStrings.java
@@ -0,0 +1,24 @@
+package Div3;
+
+import java.util.Scanner;
+
+public class PetyaAndStrings {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        String s1=in.next().toLowerCase();
+        String s2= in.next().toLowerCase();
+
+        if(!s1.equals(s2)){
+            for(int i=0;i<s1.length();i++){
+                if(s1.charAt(i)>s2.charAt(i)){
+                    System.out.println(1);
+                    break;
+                }else if(s1.charAt(i)<s2.charAt(i)){
+                    System.out.println(-1);
+                    break;
+                }
+            }
+        }else
+            System.out.println(0);
+    }
+}
