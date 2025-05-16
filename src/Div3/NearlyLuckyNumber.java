d7b23329bbc469028f1c008b9676d1aaa7ac91a1
Abdallah-Allaham <allham.abod@gmail.com>
2025-05-16 12:14:22 +0300

    problems

---
+++ src/Div3/NearlyLuckyNumber.java
@@ -0,0 +1,36 @@
+package Div3;
+
+import java.util.Scanner;
+
+public class NearlyLuckyNumber {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        String num=in.next();
+        int numOfLucky=0;
+
+        for (int i=0;i<num.length();i++){
+            if(num.charAt(i) == '4' || num.charAt(i) == '7'){
+                numOfLucky++;
+            }
+        }
+
+        boolean isLucky=true;
+
+        String numToString=Integer.toString(numOfLucky);
+
+        for(int i=0;i<numToString.length();i++){
+           if(numToString.charAt(i) !='4' && numToString.charAt(i)!='7'){
+               isLucky=false;
+               break;
+           }
+        }
+
+        if(isLucky){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+
+    }
+}
