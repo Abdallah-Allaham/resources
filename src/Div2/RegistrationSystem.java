d24873fbc3385bd3685bcac6d6f36f204b6995c6
Abdallah-Allaham <allham.abod@gmail.com>
2025-05-31 00:13:29 +0300

    HashMap

---
+++ src/Div2/RegistrationSystem.java
@@ -0,0 +1,27 @@
+package Div2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RegistrationSystem {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        Map<String,Integer> names = new HashMap<>();
+
+        for (int i=0;i<n;i++){
+            String name= in.next();
+            if (names.containsKey(name)){
+                int count=names.get(name);
+                System.out.println(name+count);
+                names.put(name,count+1);
+            }else {
+                names.put(name,1);
+                System.out.println("OK");
+            }
+        }
+    }
+}
---
+++ src/Div2/Towers.java
@@ -0,0 +1,32 @@
+package Div2;
+
+import java.util.Scanner;
+
+public class Towers {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int n= in.nextInt();
+        int[] arr = new int[1001];
+        int numOfTowers=0;
+        int max=0;
+
+        for (int i=0;i<n;i++){
+            arr[in.nextInt()]++;
+        }
+
+        for (int i=1;i<arr.length;i++){
+            if(arr[i]>0){
+                numOfTowers++;
+            }
+
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+
+        System.out.println(max + " " + numOfTowers);
+
+    }
+}
